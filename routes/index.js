e4105cf823f0c27830afdd2ba280b59d5a8e972d
Sonia <sonia.pro.neji@gmail.com>
2024-08-05 09:27:50 +0200

    backstart

--- routes/index.js
+++ routes/index.js
@@ -4,15 +4,15 @@ var router = express.Router();
 const fetch = require('node-fetch');
 
 router.get('/events', (req, res) => {
-  // fetch(`??????????`)
-  //   .then(response => response.json())
-  //   .then(data => {
-  //     if (data.status === 'ok') {
-  //       res.json({ events: data });
-  //     } else {
-  //       res.json({ events: [] });
-  //     }
-  //   });
+  fetch(`https://api.openagenda.com/v2/agendas/20500020/events?key=b74467be8ca543c5a63c1925805ca024`)
+    .then(response => response.json())
+    .then(data => {
+      if (data.status === 'ok') {
+        res.json({ events: data });
+      } else {
+        res.json({ events: [] });
+      }
+    });
 });
 
 module.exports = router;
