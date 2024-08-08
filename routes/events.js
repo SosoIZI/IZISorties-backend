51d49cc53eac494a91cff6d040d57e1425055bb3
Sonia <sonia.pro.neji@gmail.com>
2024-08-08 15:36:07 +0200

    sosobackjeudi

--- routes/events.js
+++ routes/events.js
@@ -10,38 +10,40 @@ const Event = require("../models/events");
 const Place = require("../models/places");
 
 // 1- Route qui créé un nouvel event dans Mongoose
-router.post("/:id", (req, res) => {
+router.post("/", (req, res) => {
+
+  
   const newEvent = new Event({
     eventName: req.body.eventName,
     startTime: req.body.startTime,
     endTime: req.body.endTime,
     startDate: req.body.startDate,
     endDate: req.body.endDate,
-    place: req.body.place,
-// dans le formulaire dans le frontend, l'utilisateur devra rattacher son évènement à une place
-// sinon il créera sa place dans le formulaire directement.
-// dans tous les cas, la soumission du formulaire enverra un id de place 
+    place: req.body.placeId,
+    // dans le formulaire dans le frontend, l'utilisateur devra rattacher son évènement à une place
+    // sinon il créera sa place dans le formulaire directement.
+    // dans tous les cas, la soumission du formulaire enverra un id de place
     pictures: req.body.pictures,
     description: req.body.description,
     price: req.body.price,
     categories: req.body.categories,
     nbLike: [],
     nbBooking: [],
-    user: req.params.id,
+    user: req.body.token,
   });
-// Quand je crée l'event, la BDD Place se met aussi à jour avec l'id de l'event
+  // Quand je crée l'event, la BDD Place se met aussi à jour avec l'id de l'event
   newEvent.save().then((newDoc) => {
-       Place.updateOne(
-          { _id: req.body.place },
-          { $push: { events: newDoc._id } }
-        ).then(() => {
-          res.json({ result: true });
-        });
+    Place.updateOne(
+      { _id: req.body.place },
+      { $push: { events: newDoc._id } }
+    ).then(() => {
+      res.json({ result: newEvent });
+    });
   });
 });
 
 // 2- Route qui créé dans Mongoose les events de OpenAgenda
-router.post("/openagenda", (req, res) => {
+router.post("/api/openagenda", (req, res) => {
   fetch(`https://api.openagenda.com/v2/agendas/20500020/events?key=${apiKey}`)
     .then((response) => response.json())
     .then((data) => {
@@ -58,14 +60,15 @@ router.post("/openagenda", (req, res) => {
                 { cp: infos.features[0].properties.citycode }
               ).then((placeData) => {
                 if (placeData != null) {
+                  {obj.image ? (pictures= obj.image.base + obj.image.filename) : (pictures= "/IZI_sorties_home.png")}
                   const newEvent = new Event({
                     eventName: obj.title.fr,
-                    startTime: obj.nextTiming.begin,
-                    endTime: obj.nextTiming.end,
+                    startTime: moment(obj.nextTiming.begin).format("HH:mm"),
+                    endTime: moment(obj.nextTiming.end).format("HH:mm"),
                     startDate: new Date(obj.firstTiming.begin),
                     endDate: new Date(obj.lastTiming.begin),
                     place: placeData._id,
-                    pictures: obj.image.base + obj.image.filename,
+                    pictures: pictures,                    
                     description: obj.description.fr,
                     price: "",
                     categories: ["music", "cinema"],
@@ -87,32 +90,26 @@ router.post("/openagenda", (req, res) => {
 
 // 3- Route get en fonction de l'input saisi dans la barre de recherche
 router.get("/search/:search", (req, res) => {
-  Event.aggregate([
-    {
-      $lookup: {
-        from: "places", // La collection à joindre
-        localField: "place", // Le champ local (clé étrangère)
-        foreignField: "_id", // Le champ de la collection jointe
-        as: "placeInfo", // Le nom du champ de résultat après la jointure
-      },
-    },
-    {
-      $unwind: "$placeInfo", // Décompose le tableau résultant de la jointure en documents individuels
-    },
-    {
-      $match: {
-        $or: [
-          { eventName: { $regex: req.params.search, $options: "i" } },
-          { description: { $regex: req.params.search, $options: "i" } },
-          { "placeInfo.city": { $regex: req.params.search, $options: "i" } },
-        ],
-      },
-    },
-  ]).then((data) => {
+  const searchQuery = req.params.search;
+  
+  Event.find({
+    $or: [
+      { eventName: { $regex: searchQuery, $options: "i" } },
+      { description: { $regex: searchQuery, $options: "i" } },
+      { 'place.city': { $regex: searchQuery, $options: "i" } },
+    ]
+  })
+  .populate('place')
+  .then(data => {
     console.log(data);
     res.json({ events: data });
+  })
+  .catch(error => {
+    console.error(error);
+    res.status(500).json({ error: 'Une erreur est survenue' });
   });
 });
+
 
 // géolib?
 // 4- Route get en fonction des filtres de recherche (dates, catégories, lieu)
@@ -125,13 +122,13 @@ router.get("/:startDate/:endDate/:long/:lat", (req, res) => {
   const endDateEndHeure = moment(req.params.endDate).endOf("day");
   const endDateStartHeure = moment(req.params.endDate).startOf("day");
 
-   // si on ne saisie pas de categorie, alors categories est un tableau vide
-   let categories = req.query.categorie;
-   if (!categories) {
-     categories = [];
-   } else if (!Array.isArray(categories)) {
-     categories = [categories];
-   }
+  // si on ne saisie pas de categorie, alors categories est un tableau vide
+  let categories = req.query.categorie;
+  if (!categories) {
+    categories = [];
+  } else if (!Array.isArray(categories)) {
+    categories = [categories];
+  }
 
   fetch(
     `https://api-adresse.data.gouv.fr/reverse/?lon=${req.params.long}&lat=${req.params.lat}`
@@ -199,7 +196,7 @@ router.put("/like/:idUser/:idEvent", (req, res) => {
         res.json({ result: true });
       });
     } else {
-      Tweet.updateOne(
+      Event.updateOne(
         { _id: req.params.idEvent },
         { $pull: { nbLike: req.params.idUser } }
       ).then(() => {
@@ -211,39 +208,40 @@ router.put("/like/:idUser/:idEvent", (req, res) => {
 
 // 8- Mise à jour du nombre de users qui ont booked cet event
 router.put("/booking/:idUser/:idEvent", (req, res) => {
-    Event.findOne({ _id: req.params.idEvent }).then((eventData) => {
-      if (eventData && !eventData.nbBooking.includes(req.params.idUser)) {
-        Event.updateOne(
-          { _id: req.params.idEvent },
-          { $push: { nbBooking: req.params.idUser } }
-        ).then(() => {
-          res.json({ result: true });
-        });
-      } else {
-        Tweet.updateOne(
-          { _id: req.params.idEvent },
-          { $pull: { nbBooking: req.params.idUser } }
-        ).then(() => {
-          res.json({ result: false });
-        });
-      }
-    });
+  Event.findOne({ _id: req.params.idEvent }).then((eventData) => {
+    if (eventData && !eventData.nbBooking.includes(req.params.idUser)) {
+      Event.updateOne(
+        { _id: req.params.idEvent },
+        { $push: { nbBooking: req.params.idUser } }
+      ).then(() => {
+        res.json({ result: true });
+      });
+    } else {
+      Tweet.updateOne(
+        { _id: req.params.idEvent },
+        { $pull: { nbBooking: req.params.idUser } }
+      ).then(() => {
+        res.json({ result: false });
+      });
+    }
   });
+});
 
 // 9- Route chercher un event par son id
 router.get("/:id", (req, res) => {
-  Event.findById(req.params.id).then(data => {
+  Event.findById(req.params.id).then((data) => {
     res.json({ events: data });
   });
 });
 
 // 10- Route chercher les 5 events qui ont le plus de booking
 router.get("/top/liked", (req, res) => {
-  Event.find({})
-  .then(events => {
-    const sortedEvents = events.sort((a, b) => b.nbLike.length - a.nbLike.length);
+  Event.find({}).then((events) => {
+    const sortedEvents = events.sort(
+      (a, b) => b.nbLike.length - a.nbLike.length
+    );
     res.json({ events: sortedEvents });
-  })
-})
+  });
+});
 
 module.exports = router;
